e266540f6765d7903abf111313be0d67455ff05c
Seb <sebastien@squareup.com>
2021-03-11 22:43:44 -0800

    remove headers from response

--- cmd/serve.go
+++ cmd/serve.go
@@ -32,19 +32,10 @@ var serveCmd = &cobra.Command{
 			defer c.Request().Body.Close()
 			content, _ := ioutil.ReadAll(c.Request().Body)
 
-			h := http.Header{}
-			for name, values := range c.Request().Header {
-				if strings.HasPrefix(strings.ToLower(name), "x-") {
-					// Heroku adds a lot of these that throw things off
-					continue
-				}
-				h[name] = values
-			}
 			return c.JSON(http.StatusOK, map[string]interface{}{
-				"method":  c.Request().Method,
-				"uri":     c.Request().URL.RequestURI(),
-				"body":    string(content),
-				"headers": h,
+				"method": c.Request().Method,
+				"uri":    c.Request().URL.RequestURI(),
+				"body":   string(content),
 			})
 		})
 		port := os.Getenv("PORT")
